e4630f372ddd6465198b268cae9a3f9304c0b192
arisslee <87521778+arisslee@users.noreply.github.com>
2023-12-22 16:46:54 +0800

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/flutter_demo/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.LifeSpring
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity: FlutterFragmentActivity () {
+}
